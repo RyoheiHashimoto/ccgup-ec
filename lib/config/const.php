4e7e8de7be1130a26251695acad7a221ed60678a
RyoheiHashimoto <51049810+RyoheiHashimoto@users.noreply.github.com>
2019-10-03 14:37:46 +0900

    DB接続情報を削除

--- lib/config/const.php
+++ lib/config/const.php
@@ -5,12 +5,6 @@
  * @copyright CodeCamp https://codecamp.jp
  */
 
-// MySQL接続情報
-define('DB_USER', 'codecamp');      // MySQLのユーザ名
-define('DB_PASS', 'Sk11owvGe');     // MySQLのパスワード
-define('DB_NAME', 'codecamp_ec');   // データベース名
-define('DB_HOST', 'localhost');     // データベースのホスト名又はIPアドレス
-
 define("DIR_APP", dirname(dirname(__FILE__)) . '/');    // システムのベースディレクトリ
 define("DIR_IMG",  "./img/");                           // 画像ディレクトリのパス（webrootからの相対)
 define("DIR_IMG_FULL", DIR_APP . "webroot/img/");       // 画像ディレクトリのフルパス
